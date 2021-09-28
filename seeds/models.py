1c250daf8fb2c2d3ef42d16c341d4fafa125b853
Mike <mleo40@gmail.com>
2021-09-28 19:40:00 -0400

    padding out the seed model

--- seeds/models.py
+++ seeds/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class seeds(models.Model):
+    """
+    Creates dbase data for seeds
+    """
+    light_options = [(1, "shade"),
+                     (2, "partial sun"),
+                     (3, "full sun")
+                     ]
+
+    name = models.CharField(max_lenght=64)
+    subname = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    germination = models.IntegerField()
+    date_on_packet = models.DateField()
+    depth = models.IntegerField()
+    light = models.Choices(select=light_options)
+    spacing = models.IntegerField()
+    harvest = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} {self.subname}"
